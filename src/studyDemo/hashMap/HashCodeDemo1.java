ab7e4b3b78111664fc93660ad78ef0a41a6e3807
yuanbaojian <312ybj@gmail.com>
2020-05-07 09:10:14 +0800

    Initial commit

---
+++ src/studyDemo/hashMap/HashCodeDemo1.java
@@ -0,0 +1,19 @@
+package studyDemo.hashMap;
+
+import java.util.HashMap;
+
+/**
+ * @Author hashCodeDemo1
+ * @Description //TODO $
+ * @Date $ $
+ * @Param $
+ * @return $
+ **/
+public class HashCodeDemo1 {
+
+    public static void main(String[] args) {
+        HashMap hashMap = new HashMap();
+        hashMap.put("hello", "world");
+        System.out.println("hashMap = " + hashMap);
+    }
+}
